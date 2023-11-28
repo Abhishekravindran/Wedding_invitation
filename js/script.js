9a140025321fec064878c2cfc6adf264e483d89b
Abhishekravindran <35551932+Abhishekravindran@users.noreply.github.com>
2023-11-28 11:23:50 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-$(document).ready(function(){
+$(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
